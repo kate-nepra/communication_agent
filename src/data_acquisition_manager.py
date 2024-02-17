7d9d17e2c59e070750a56dc69af74e6a770246ab
rinaen <k.nepra@gmail.com>
2024-02-17 17:03:58 +0100

    Refactoring 1

--- src/data_acquisition_manager.py
+++ src/data_acquisition_manager.py
@@ -24,31 +24,23 @@ class DataAcquisitionManager:
     def __init__(self, sources_db: SourcesDB):
         self._sources_db = sources_db
 
-    def acquire_data(self) -> None:
+    def _crawl_url(self, url: str, parents: list):
+        wc = WebCrawler(url, parents)
+        extend_df = wc.get_extend_df()
+        existing_urls = self._sources_db.get_existing_from_list(extend_df[URL])
+        self._update_existing_urls(existing_urls)
+        return extend_df[~extend_df[URL].isin(existing_urls)]
+
+    def acquire_data(self, urls_df) -> None:
         """
         This method is the main entry point for the data acquisition process. It retrieves urls from the sources database
         and passes them to the web crawler and the web scraper.
         """
 
-        urls_df = self._sources_db.get_all_crawl_only_sources()
-        for url in urls_df[URL]:
-            wc = WebCrawler(url, urls_df['parent'])
-            extend_df = wc.get_extend_df()
-
-    def initial_data_acquisition(self) -> None:
-        """
-        This method is responsible for the initial data acquisition. It retrieves the initial set of urls from the
-        sources database and passes them to the web crawler and the web scraper.
-        """
-
-        urls_df = self._sources_db.get_all_crawl_only_sources()  # get also non crawl only later
-        for url in urls_df[URL]:
-            wc = WebCrawler(url, urls_df['parent'])
-            extend_df = wc.get_extend_df()
-            existing_urls = self._sources_db.get_existing_from_list(extend_df[URL])
-            self._update_existing_urls(existing_urls)
-
-            extend_df = extend_df[~extend_df[URL].isin(existing_urls)]
+        crawl_only_df = urls_df[urls_df[CRAWL_ONLY] == 1]
+        not_crawl_only_df = urls_df[urls_df[CRAWL_ONLY] == 0]
+        for url in crawl_only_df[URL]:
+            extend_df = self._crawl_url(url, self._sources_db.get_all_parents())
             for new_url in extend_df[URL]:
                 ws = WebScraper(new_url)
                 if not ws.does_html_contain_substrs(BRNO_SUBSTRS):
@@ -64,8 +56,33 @@ class DataAcquisitionManager:
             extend_df = extend_df[~extend_df[URL].isin(BLACK_LIST)]
             self._sources_db.insert_sources(extend_df)
 
+        for url in not_crawl_only_df[URL]:
+            ws = WebScraper(url)
+            if not ws.does_html_contain_substrs(BRNO_SUBSTRS):
+                BLACK_LIST.append(url)
+                continue
+            content = get_parsed_content(ws.get_clean_text())
+            type_name = content.record_type
+            not_crawl_only_df.loc[not_crawl_only_df[URL] == url, TYPE_ID] = self._sources_db.get_type_id(type_name)
+
+    def initial_data_acquisition(self) -> None:
+        """
+        This method is responsible for the initial data acquisition. It retrieves the initial set of urls from the
+        sources database and passes them to the web crawler and the web scraper.
+        """
+
+        urls_df = self._sources_db.get_all_sources()
+        self.acquire_data(urls_df)
+
     def _update_existing_urls(self, existing_urls) -> None:
         """
         This method updates the existing urls in the sources' database. It sets the date_added to current date.
         """
         self._sources_db.update_existing_urls_date(existing_urls, arrow.now().format(DATE_FORMAT))
+
+
+if __name__ == '__main__':
+    sources = SourcesDB()
+    dam = DataAcquisitionManager(sources)
+    dam.initial_data_acquisition()
+    sources.close_connection()
