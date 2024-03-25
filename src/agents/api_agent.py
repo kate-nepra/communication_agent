aa508092dcb56844c8e58ce0d4bf664248ce6030
rinaen <k.nepra@gmail.com>
2024-03-25 18:14:16 +0100

    Rework parser to use ApiAgent

--- src/agents/api_agent.py
+++ src/agents/api_agent.py
@@ -6,6 +6,7 @@ import instructor
 from langchain_core.utils.function_calling import convert_to_openai_tool
 from openai import OpenAI
 from pydantic import BaseModel
+from json_repair import repair_json
 
 
 @dataclass
@@ -27,22 +28,28 @@ class ApiAgent(ABC):
         self.client = OpenAI(api_key=api_key, base_url=url)
         self.model_name = model_name
 
-    def get_function_call_response(self, module: dict, functions: list, messages: list[Message]):
+    def get_function_call_response(self, module: dict, functions: list, messages: list[Message], max_retries: int = 2):
         """
         Instruct the model to call a function and get the result of the function call
         :param module: Module containing the functions
         :param functions: List of functions to chosen from
         :param messages: List of messages to be sent to the model
+        :param max_retries:
         :return: result of called function
         """
-        messages = [asdict(m) for m in messages]
+        con_messages = [asdict(m) for m in messages]
         openai_functions = [self._transfer_function_to_openai_function_schema(f) for f in functions]
         response = self.client.chat.completions.create(
             model=self.model_name,
-            messages=messages,
+            messages=con_messages,
             functions=openai_functions,
             stream=False
         )
+        if response.choices[0].message.function_call is None:
+            if max_retries > 0:
+                return self.get_function_call_response(module, functions, messages, max_retries - 1)
+            raise ValueError("Function call not found in the response")
+
         return self._parse_function_call(module, response)
 
     @abstractmethod
@@ -54,6 +61,29 @@ class ApiAgent(ABC):
         :return: result of called function
         """
         pass
+
+    @staticmethod
+    def _parse_function_arguments(function_arguments: dict) -> dict:
+        """
+        Parse the individual function arguments from the response
+        :param function_arguments: Unparsed function arguments in JSON string format
+        :return: dictionary containing the parsed function arguments
+        """
+
+        if isinstance(function_arguments, dict):
+            function_arguments = dict(function_arguments)
+        else:
+            a = str(function_arguments)
+            a = a.replace('\"https\"', 'https')
+            a = a.replace('None', '""')
+            a = repair_json(a)
+            function_arguments = dict(json.loads(a))
+        if "description" not in str(function_arguments):
+            return function_arguments
+        result = {}
+        for key in function_arguments:
+            result[key] = function_arguments[key].get("description", "")
+        return result
 
     @staticmethod
     def _get_function_parameters(module: dict, func_name):
@@ -69,7 +99,7 @@ class ApiAgent(ABC):
             parameter_names = list(signature.parameters.keys())
             return parameter_names
         else:
-            return []
+            raise ValueError(f"Function {func_name} not found in the module")
 
     @abstractmethod
     def get_json_format_response(self, response_model: BaseModel, messages: list[Message]) -> dict:
@@ -88,7 +118,11 @@ class ApiAgent(ABC):
         :param function: Function to be converted
         :return: dictionary containing the function schema
         """
-        return dict(convert_to_openai_tool(function)["function"])
+        try:
+            converted = dict(convert_to_openai_tool(function)["function"])
+        except Exception as e:
+            raise ValueError(f"Error converting function to OpenAI function schema: {e}")
+        return converted
 
 
 class LlamaApiAgent(ApiAgent):
@@ -100,28 +134,13 @@ class LlamaApiAgent(ApiAgent):
         :param response: Model response in API format
         :return: result of called function
         """
+
         function_name = response.choices[0].message.function_call.name
         if not self._get_function_parameters(module, function_name):
             return module[function_name]()
         function_arguments = self._parse_function_arguments(response.choices[0].message.function_call.arguments)
+        # todo check if parameters are correct
         return module[function_name](**function_arguments)
-
-    @staticmethod
-    def _parse_function_arguments(function_arguments: dict) -> dict:
-        """
-        Parse the individual function arguments from the response
-        :param function_arguments: Unparsed function arguments in JSON string format
-        :return: dictionary containing the parsed function arguments
-        """
-        # a = str(function_arguments)
-        # a = a.replace('\"https\"', 'https')
-        # a = a.replace('None', '""')
-        # data = json.loads(a)
-        function_arguments = dict(function_arguments)
-        result = {}
-        for key in function_arguments:
-            result[key] = function_arguments[key].get("description", "")
-        return result
 
     def get_json_format_response(self, response_model: BaseModel, messages: list[Message]) -> dict:
         """
@@ -154,7 +173,7 @@ class OpenAIApiAgent(ApiAgent):
         function_name = response.choices[0].message.function_call.name
         if not self._get_function_parameters(module, function_name):
             return module[function_name]()
-        function_arguments = response.choices[0].message.function_call.arguments
+        function_arguments = self._parse_function_arguments(response.choices[0].message.function_call.arguments)
         return module[function_name](**function_arguments)
 
     def get_json_format_response(self, response_model: BaseModel, messages: list[Message]) -> dict:
---
+++ src/content_parsing.py
@@ -0,0 +1,76 @@
+from dataclasses import dataclass
+import arrow
+from dotenv import load_dotenv
+
+from src.agents.api_agent import ApiAgent, Message
+from src.constants import DATE_FORMAT
+
+load_dotenv()
+
+
+@dataclass
+class Duration():
+    """ Format for event duration. Field "end" is optional, it is used for period of time (that are two dates from-to). Use the format YYYY-MM-DD for date, and format HH:MM:SS for time."""
+    start: str
+    end: str = None
+
+
+@dataclass
+class BaseSchema():
+    """ Base schema for all entities """
+    header: str
+    record_type: str
+    brief: str
+    text: str
+    url: str
+    date_fetched: str
+
+
+@dataclass
+class ExtendedSchema(BaseSchema):
+    """ Extended schema for places and administrative entities """
+    address: str
+
+
+@dataclass
+class EventSchema(BaseSchema):
+    """ Event schema for events """
+    address: str
+    dates: list[str]
+
+
+def get_parsed_content_by_function_call(agent: ApiAgent, url: str, content: str) -> BaseSchema:
+    def add_place(header: str, text: str, brief: str, url: str, address: str) -> BaseSchema:
+        """ Call this function if you encounter entity that is a place or destination in or near Brno city, such as restaurant, café, bar, bakery, museum, tour, greenery, church, castle, university, kino, theatre or similar."""
+        return ExtendedSchema(header, "place", brief, text, url, arrow.now().format(DATE_FORMAT), address)
+
+    def add_event(header: str, text: str, brief: str, address: str, url: str, dates: [str]) -> BaseSchema:
+        """ Call this function if you encounter entity that is an event such as concert, exhibition, celebration, festival, sports match, theatrical performance or similar."""
+        # Todo check if dates are in correct format
+        return EventSchema(header, "event", brief, text, url, arrow.now().format(DATE_FORMAT), address, dates)
+
+    def add_administration(header: str, text: str, brief: str, url: str, address: str) -> BaseSchema:
+        """ Call this function if you encounter entity that is administrative information such as Municipal office, business, authorities, insurance, social Care, vehicle registration, taxes, fees, information for expats, school system, residence, ID cards or similar."""
+        return ExtendedSchema(header, "administration", brief, text, url, arrow.now().format(DATE_FORMAT), address)
+
+    def add_static(header: str, text: str, brief: str, url: str) -> BaseSchema:
+        """ Call this function if you encounter entity that contains blog post, an article from wikipedia or information about well-known personality connected with Brno that is not likely to change in next 5 years. This entity does not contain any information about places in Brno, events or administrative."""
+        return BaseSchema(header, "static", brief, text, url, arrow.now().format(DATE_FORMAT))
+
+    dates = [{"start": "2024-01-11"}, {"start": "2024-01-14 15:00"}, {"start": "2024-01-31 15:00", "end": "2024-02-14"}]
+    messages = [Message(role="system",
+                        content=f""" You are a smart processor of web-scraped text. Follow these instructions:
+      1. Go through the text and extract information from the article, translate to English if not in English. Do NOT use escape characters, transfer all special encoding like Unicode to UTF-8 (plain text). 
+      2. Use the function with the most fitting description, pass parameters as described:
+        Use provided or generate a header more fitting the found text.
+        If you encounter a descriptive text of the entity, for example a plot of a theatrical performance for an event or a menu of a restaurant for a place, assign it as the text.
+        Create a brief which is a sum up of the text no longer than 3 sentences.
+        Assign provided url as url.
+        If you encounter address of a place (such as address of a municipal office for administration or address of concert-hall for an event), assign is as address. Fill in "Not found" if address not found but required.
+        For an event, assign date(s) of the event as list of durations. The duration format is a JSON object with fields "start" and "end". Field "end" is optional, it is used for period of time (that are two dates from-to, like startdate-enddate, for example 31 jan–14 feb 2024). Use the format YYYY-MM-DD for date, and format HH:MM:SS for time. For example {dates}.
+      3. End with the function call response, the function call must be in valid JSON format.
+    URL is {url}. 
+    Here is the text to process ```{content}```""")]
+
+    return agent.get_function_call_response(locals(), [add_place, add_administration, add_static, add_event],
+                                            messages)  # Todo catch errs
